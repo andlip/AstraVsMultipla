f73f3123cd78767980ccf2fbe54cabd85452fe0b
Andrzej Lipiec <andrew.lipiec@gmail.com>
2019-04-09 18:39:32 +0200

    Extending Readme file

---
+++ DL_object_detection.py
@@ -0,0 +1,96 @@
+# import the necessary packages
+import numpy as np
+import argparse
+import cv2
+
+def insert_str(string, str_to_insert, index):
+    return string[:index] + str_to_insert + string[index:]
+
+# construct the argument parse and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--image", default = "/home/andrzej/Python/DeepLearn/images/astra/111.jpg",
+	help="path to input image")
+ap.add_argument("-o", "--output", help = "path to output cropped file")
+ap.add_argument("-p", "--prototxt", default="./MobileNetSSD_deploy.prototxt.txt",
+	help="path to Caffe 'deploy' prototxt file")
+ap.add_argument("-m", "--model",  default = "./MobileNetSSD_deploy.caffemodel",
+	help="path to Caffe pre-trained model")
+ap.add_argument("-c", "--confidence", type=float, default=0.8,
+	help="minimum probability to filter weak detections")
+args = vars(ap.parse_args())
+
+# initialize the list of class labels MobileNet SSD was trained to
+# detect, then generate a set of bounding box colors for each class
+CLASSES = ["background", "aeroplane", "bicycle", "bird", "boat",
+	"bottle", "bus", "car", "cat", "chair", "cow", "diningtable",
+	"dog", "horse", "motorbike", "person", "pottedplant", "sheep",
+	"sofa", "train", "tvmonitor"]
+COLORS = np.random.uniform(0, 255, size=(len(CLASSES), 3))
+
+# load our serialized model from disk
+print("[INFO] loading model...")
+net = cv2.dnn.readNetFromCaffe(args["prototxt"], args["model"])
+
+# load the input image and construct an input blob for the image
+# by resizing to a fixed 300x300 pixels and then normalizing it
+# (note: normalization is done via the authors of the MobileNet SSD
+# implementation)
+image = cv2.imread(args["image"])
+(h, w) = image.shape[:2]
+blob = cv2.dnn.blobFromImage(cv2.resize(image, (300, 300)), 0.007843,
+	(300, 300), 127.5)
+
+
+# pass the blob through the network and obtain the detections and
+# predictions
+print("[INFO] computing object detections...")
+net.setInput(blob)
+detections = net.forward()
+
+
+
+listOfImages = []
+# loop over the detections
+for i in np.arange(0, detections.shape[2]):
+	# extract the confidence (i.e., probability) associated with the
+	# prediction
+	confidence = detections[0, 0, i, 2]
+
+	# filter out weak detections by ensuring the `confidence` is
+	# greater than the minimum confidence
+	if confidence > args["confidence"]:
+		# extract the index of the class label from the `detections`,
+		# then compute the (x, y)-coordinates of the bounding box for
+		# the object
+		idx = int(detections[0, 0, i, 1])
+		box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+		(startX, startY, endX, endY) = box.astype("int")
+
+		if (CLASSES[idx] == "car"):
+			print("Got a new car!")
+			cropped = image[startY:endY,startX:endX]
+			listOfImages.append(cropped)
+
+		# display the prediction
+		label = "{}: {:.2f}%".format(CLASSES[idx], confidence * 100)
+		print("[INFO] {}".format(label))
+		cv2.rectangle(image, (startX, startY), (endX, endY),
+			COLORS[idx], 2)
+		y = startY - 15 if startY - 15 > 15 else startY + 15
+		cv2.putText(image, label, (startX, y),
+			cv2.FONT_HERSHEY_SIMPLEX, 0.5, COLORS[idx], 2)
+			
+# show the output image
+if not (len(listOfImages) < 1):
+	for j in range(len(listOfImages)):
+		
+		fileName = "/home/andrzej/Python/DeepLearn/{}".format(args["output"])
+		fileName = insert_str(fileName, "_{}".format(j), len(fileName) - 4)
+		print(fileName)
+		cv2.imwrite(fileName, listOfImages[j])
+		#cv2.imshow("Output", listOfImages[j])
+		#cv2.waitKey(0)
+else:
+	print("No cars in pic {}".format(args["input"]))
+#cv2.imshow("Output", image)
+
--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # AstraVsMultipla
 For testing reinforcement learning
+
+General workflow:
+
+- use JS commands from the browser
+	+ navigate to google search -> Opel Astra -> images -> sroll down -> scroll even more -> execute JS commands
+	+ output will be list of images urls in a file "urls.txt"
+
+- use download_images.py to... download images
+	+ -u <path to urls.txt file>
+	+ -o <output path>
+	+ files are downlowaded as 00000xxx.jpg to the outpu path
+
+- use PrepareData.py to select and crop pictures containing cars
+	+ assumes pictures are organised as: images/astra/xxx.jpg and images/clio/xxx.jpg 
